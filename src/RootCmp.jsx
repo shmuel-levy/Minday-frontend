ebe1420e9e23cfb76ad53d6588353840103d54ec
shmuel-levy <shmuellevy16@gmail.com>
2025-05-28 09:30:09 +0300

    convert car starter to Monday clone base structure

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -4,12 +4,11 @@ import { Routes, Route, Navigate } from 'react-router'
 import { userService } from './services/user'
 import { HomePage } from './pages/HomePage'
 import { AboutUs, AboutTeam, AboutVision } from './pages/AboutUs'
-import { CarIndex } from './pages/CarIndex.jsx'
-import { ReviewIndex } from './pages/ReviewIndex.jsx'
+import { BoardIndex } from './pages/BoardIndex.jsx'
 import { ChatApp } from './pages/Chat.jsx'
 import { AdminIndex } from './pages/AdminIndex.jsx'
 
-import { CarDetails } from './pages/CarDetails'
+import { BoardDetails } from './pages/BoardDetails'
 import { UserDetails } from './pages/UserDetails'
 
 import { AppHeader } from './cmps/AppHeader'
@@ -18,7 +17,6 @@ import { UserMsg } from './cmps/UserMsg.jsx'
 import { LoginSignup } from './pages/LoginSignup.jsx'
 import { Login } from './pages/Login.jsx'
 import { Signup } from './pages/Signup.jsx'
-
 
 export function RootCmp() {
     return (
@@ -33,10 +31,9 @@ export function RootCmp() {
                         <Route path="team" element={<AboutTeam />} />
                         <Route path="vision" element={<AboutVision />} />
                     </Route>
-                    <Route path="car" element={<CarIndex />} />
-                    <Route path="car/:carId" element={<CarDetails />} />
+                    <Route path="board" element={<BoardIndex />} />
+                    <Route path="board/:boardId" element={<BoardDetails />} />
                     <Route path="user/:id" element={<UserDetails />} />
-                    <Route path="review" element={<ReviewIndex />} />
                     <Route path="chat" element={<ChatApp />} />
                     <Route path="admin" element={
                         <AuthGuard checkAdmin={true}>
@@ -53,9 +50,6 @@ export function RootCmp() {
         </div>
     )
 }
-
-
-
 
 function AuthGuard({ children, checkAdmin = false }) {
     const user = userService.getLoggedinUser()
