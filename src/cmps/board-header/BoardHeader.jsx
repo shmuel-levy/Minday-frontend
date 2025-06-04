6c4fb6d1bccaf66a71976329ce7e0a3f00ec50ff
shmuel-levy <shmuellevy16@gmail.com>
2025-06-04 15:11:48 +0300

    Add board header components with Monday.com styling
    - Create BoardHeader, BoardTitleSection, BoardActions, ViewControls, TableControls
    - Add board title dropdown with edit functionality
    - Add proper button styling with hover effects

--- src/cmps/board-header/BoardHeader.jsx
+++ src/cmps/board-header/BoardHeader.jsx
@@ -3,23 +3,22 @@ import { BoardActions } from './BoardActions'
 import { ViewControls } from './ViewControls'
 import { TableControls } from './TableControls'
 
-export function BoardHeader({ board, onUpdateBoard }) {
+export function BoardHeader({ board, onUpdateBoard, onAddNewTask, onAddNewGroup }) {
     return (
         <div className="board-header">
             <div className="board-header-top">
                 <BoardTitleSection 
-                    board={board} 
-                    onUpdateBoard={onUpdateBoard} 
+                    board={board}
+                    onUpdateBoard={onUpdateBoard}
                 />
-                
                 <div className="board-header-right">
                     <BoardActions />
                 </div>
             </div>
-
+            
             <div className="board-header-bottom">
                 <ViewControls />
-                <TableControls />
+                <TableControls onAddNewTask={onAddNewTask} onAddNewGroup={onAddNewGroup} />
             </div>
         </div>
     )
