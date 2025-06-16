6dd235ef7bb4a9cd5db123ccbc4cd7a092b9d955
Shoham Shtiler <shtilershoham@gmail.com>
2025-06-16 20:56:36 +0300

    Fix .col-members inside progress-bar

--- src/cmps/table/column-types/MembersDistribution.jsx
+++ src/cmps/table/column-types/MembersDistribution.jsx
@@ -1,4 +1,5 @@
 import { Avatar } from './MembersColumn'
+import { useState } from 'react';
 
 export function MembersDistribution({ tasks }) {
     const members = tasks.flatMap(task => task.members || []);
@@ -12,24 +13,28 @@ export function MembersDistribution({ tasks }) {
     return (
         <div className="members-summary">
             {totalMembers === 0 ? (
-                <img 
-                    src="https://cdn.monday.com/icons/dapulse-person-column.svg" 
-                    width="32" 
-                    height="32" 
+                <img
+                    src="https://cdn.monday.com/icons/dapulse-person-column.svg"
+                    width="30"
+                    height="30"
                     alt="Default avatar"
                 />
             ) : totalMembers === 1 ? (
-                <Avatar member={uniqueMembers[0]} size="32px" />
-            ) : (
-                <div style={{display: 'flex', alignItems: 'center'}}>
-                    {uniqueMembers.slice(0, 3).map((member, i) => (
-                        <div key={member._id} style={{marginLeft: i > 0 ? '-8px' : '0', zIndex: 3-i}}>
-                            <Avatar member={member} size="24px" />
+                <Avatar member={uniqueMembers[0]} size="30px" />
+            ) : totalMembers === 2 ? (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    {uniqueMembers.slice(0, 2).map((member, i) => (
+                        <div key={member._id} style={{ marginLeft: i > 0 ? '-8px' : '0', zIndex: 3 + i }}>
+                            <Avatar member={member} size="30px" />
                         </div>
                     ))}
-                    {totalMembers > 3 && (
-                        <div className="counter">+{totalMembers - 3}</div>
-                    )}
+                </div>
+            ) : (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <div style={{ marginLeft: '0', zIndex: 3 }}>
+                        <Avatar member={uniqueMembers[0]} size="30px" />
+                    </div>
+                    <div className="counter">+{totalMembers - 1}</div>
                 </div>
             )}
         </div>
