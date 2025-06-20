ba2371e2f551a9a7b2ab88ed6bba90fdc16ab428
shmuel-levy <shmuellevy16@gmail.com>
2025-06-20 23:22:49 +0300

    Create basic dashboard component

---
+++ src/pages/BoardDashboard.jsx
@@ -0,0 +1,10 @@
+export function BoardDashboard({ board }) {
+    return (
+        <div className="board-dashboard">
+            <div className="dashboard-content">
+                <h2>Dashboard View</h2>
+                <p>Dashboard content will go here</p>
+            </div>
+        </div>
+    )
+}
