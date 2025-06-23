d105d4376bd0d9608534e6d27f7c9de1216b06a6
Shoham Shtiler <shtilershoham@gmail.com>
2025-06-23 16:39:21 +0300

    Add style to home page - app header

---
+++ src/cmps/svg/HomeArrowIcon.jsx
@@ -0,0 +1,18 @@
+export function HomeArrowIcon(props) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="10"
+            height="14"
+            viewBox="0 0 9 7"
+            {...props}
+        >
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M4.628.616a.5.5 0 1 0-.64.768L6.203 3.23H.5a.5.5 0 0 0 0 1h5.612L3.988 6a.5.5 0 1 0 .64.769l3.23-2.693a.5.5 0 0 0 0-.768z"
+            />
+        </svg>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,59 @@
+import { useEffect, useRef } from 'react'
+import * as Ladda from 'ladda'
+import 'ladda/dist/ladda.min.css'
+
 import mindayLogo from '../../src/assets/img/minday-logo.png'
 import { useNavigate } from 'react-router-dom'
+import { HomeArrowIcon } from '../cmps/svg/HomeArrowIcon'
+
 export function HomePage() {
 
     const navigate = useNavigate()
+    const laddaRef = useRef(null)
+    let laddaInstance = useRef(null)
+
+    useEffect(() => {
+        if (laddaRef.current) {
+            laddaInstance.current = Ladda.create(laddaRef.current)
+        }
+    }, [])
 
     function handleGetStarted() {
-        navigate('/board')
+        if (laddaInstance.current) {
+            laddaInstance.current.start()
+        }
+
+        setTimeout(() => {
+            if (laddaInstance.current) {
+                laddaInstance.current.stop()
+            }
+            navigate('/board')
+        }, 1000)
     }
+
     return (
         <section className="app-header-wraper">
             <div className="home-app-header">
-                <img src={mindayLogo} alt="Minday Logo" className="logo-img"
-                />
+                <img src={mindayLogo} alt="Minday Logo" className="logo-img" />
                 <nav className="navbar">
-                    <button className="login-btn">Login</button>
-                    <div className="get-started" onClick={handleGetStarted}>Get Started </div>
+                    <button className="login-btn">Log in</button>
+                    <div className="get-started-wraper" onClick={handleGetStarted}>
+                        <button
+                            ref={laddaRef}
+                            className="get-started-btn"
+                            data-style="slide-right"
+                        >
+                            <span className="get-started">
+                                <span>Get Started</span>
+                                <span className="arrow-icon">
+                                    <HomeArrowIcon />
+                                </span>
+                            </span>
+                            <span className="ladda-spinner"></span>
+                        </button>
+                    </div>
                 </nav>
             </div>
-
         </section>
     )
 }
