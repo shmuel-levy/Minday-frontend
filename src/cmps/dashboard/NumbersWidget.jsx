90e38048a3ee8bf1483a80c0d460f29a3f9760a8
AgamMorLevi <agammor96@gmail.com>
2025-07-03 08:58:34 +0300

    dashboard

--- src/cmps/dashboard/NumbersWidget.jsx
+++ src/cmps/dashboard/NumbersWidget.jsx
@@ -1,8 +1,10 @@
+import CountUp from 'react-countup';
+
 export function NumbersWidget({ title, value }) {
   return (
     <div className="numbers-widget">
       <h2>{title}</h2>
-      <div className="number-value">{value || 0}</div>
+      <div className="number-value">{value}</div>
     </div>
   );
 }
